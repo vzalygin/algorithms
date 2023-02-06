1f929174e83d84186e0b7de270f9a7679d0085bb
Vyacheslav Zalygin <vya.zalygin@gmail.com>
2023-02-06 15:02:08 +0300

    2289 fail

--- 2289_steps_to_make_array_non-decreasing.go
+++ 2289_steps_to_make_array_non-decreasing.go
@@ -1,35 +1,12 @@
 package main
 
-func max(nums ...int) int {
-	var m int
-	for _, n := range nums {
-		if m < n {
-			m = n
-		}
-	}
-	return m
-}
-
+/*
+test cases
+f := []int{5, 3, 4, 4, 7, 3, 6, 11, 8, 5, 11} // 3
+e := []int{7, 14, 4, 14, 13, 2, 6, 13}        // 3
+t := []int{10, 1, 2, 3, 4, 5, 6, 1, 2, 3}     // 6
+r := []int{5, 14, 15, 2, 11, 5, 13, 15}       // 3
+*/
 func totalSteps(nums []int) int {
-	maxSteps := 0
-	steps := 0
-	last := 0
-	lastMax := 0
-
-	for _, num := range nums {
-		if lastMax <= num { // последовательность неубывает
-			maxSteps = max(maxSteps, steps)
-			steps = 0
-			lastMax = num
-		} else {
-			if last > num && last != lastMax {
-			} else {
-				steps += 1
-			}
-		}
-		last = num
-	}
-
-	maxSteps = max(maxSteps, steps)
-	return maxSteps
+	return 0
 }
